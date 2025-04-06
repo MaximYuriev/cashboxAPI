bda0661078eb17d1ec94b3acd4118e51a591896f
Maxim <yrev2011@gmail.com>
2025-04-06 19:33:40 +0700

    Delete entity id on base entity

--- app/domain/entities/base.py
+++ app/domain/entities/base.py
@@ -1,4 +1,3 @@
-import uuid
 from abc import ABC
 from dataclasses import dataclass, field
 from datetime import datetime, UTC
@@ -6,6 +5,5 @@ from datetime import datetime, UTC
 
 @dataclass
 class BaseEntity(ABC):
-    entity_id: uuid.UUID = field(default_factory=uuid.uuid4, kw_only=True)
     created_at: datetime = field(default_factory=lambda: datetime.now(UTC), kw_only=True)
     updated_at: datetime = field(default_factory=lambda: datetime.now(UTC), kw_only=True)
--- app/domain/entities/product.py
+++ app/domain/entities/product.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+import uuid
+from dataclasses import dataclass, field
 
 from app.domain.entities.base import BaseEntity
 from app.domain.values.numbers import NonNegativeInt, Price
@@ -7,6 +8,7 @@ from app.domain.values.texts import Text, Name
 
 @dataclass
 class Product(BaseEntity):
+    product_id: uuid.UUID = field(default_factory=uuid.uuid4, kw_only=True)
     name: Name
     description: Text
     quantity: NonNegativeInt
